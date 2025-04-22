cd2447c35ca79de61cb7255271d2966603273e61
Dyson <dysonkui@gmail.com>
2025-04-22 11:06:57 +0400

    Started working on agentState

---
+++ Towergeist/Assets/Code/Agent/AgentState.cs
@@ -0,0 +1,10 @@
+using JW.Grid.GOAP;
+using UnityEngine;
+
+namespace Dyson.Towergeist
+{
+    public abstract class AgentState : MonoBehaviour
+    {
+        public abstract void PerformAction();
+    }
+}
---
+++ Towergeist/Assets/Code/Agent/AgentTest.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Dyson.Towergeist
+{
+    public class AgentTest : AgentState
+    {
+        public override void PerformAction()
+        {
+            Debug.Log("Performing test perform performance");
+        }
+    }
+}
