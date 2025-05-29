632b8782f32490c4da8b31eca61f1eee52cff970
Dyson <dysonkui@gmail.com>
2025-05-29 15:18:13 +0400

    Added a bunch of assets + speed up button

---
+++ Towergeist/Assets/_Dyson/Scripts/SpeedGame.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedGame : MonoBehaviour
+{
+    public void SpeedUpGame()
+    {
+        Time.timeScale++;
+    }
+}
