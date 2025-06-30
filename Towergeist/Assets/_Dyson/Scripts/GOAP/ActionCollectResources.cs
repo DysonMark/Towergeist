33a3bba7fcfacf7781c309a2d24aeb68b4f9f397
Dyson <dysonkui@gmail.com>
2025-07-01 00:39:19 +0400

    Code reformatting

--- Towergeist/Assets/_Dyson/Scripts/GOAP/ActionCollectResources.cs
+++ Towergeist/Assets/_Dyson/Scripts/GOAP/ActionCollectResources.cs
@@ -9,40 +9,107 @@ using Resources;
 using JW.Grid.GOAP.Goals;
 using Movement;
 
+
 namespace Dyson.Towergeist
 {
+    /// <summary>
+    /// GOAP Action for resource collection behavior.
+    /// Coordinates agent movement between gathering and working areas based on resource thresholds.
+    /// </summary>
     public class ActionCollectResources : ActionBase
     {
-        [Header("Collect Action")] 
-        private List<Type> supportedGoals = new List<Type>() { typeof(GoalWork) };
-        
-        [Header("Resources")]
-        [SerializeField] private float _resourceTargetAmount;
+        #region Serialized Fields
+        [Header("Resource Configuration")]
+        [SerializeField] private float resourceTargetAmount = 10f;
+        #endregion
+
+        #region Private Fields
+        private readonly List<Type> supportedGoals = new List<Type> { typeof(GoalWork) };
+        #endregion
+
+        #region Public Properties
+        /// <summary>
+        /// Gets the current resource target amount for this action.
+        /// </summary>
+        public float ResourceTargetAmount => resourceTargetAmount;
+        #endregion
+
+        #region GOAP Action Interface
+        /// <summary>
+        /// Returns the list of goals this action can fulfill.
+        /// </summary>
         public override List<Type> GetSupportedGoals()
         {
             return supportedGoals;
         }
+
+        /// <summary>
+        /// Initiates the resource collection process by moving to the gathering area.
+        /// </summary>
         public override void OnActivated()
         {
-            // Move to resource site
-            moveSystem.MoveTo(AreaMover.Destination.GatheringArea);
-            // If we have enough resources, drop them off at the tower
-            
-            Debug.Log("Activation of collecting resources");
+            MoveToGatheringArea();
         }
 
+        /// <summary>
+        /// Updates the action state each frame, managing transitions between gathering and working.
+        /// </summary>
+        /// <param name="dt">Delta time since last update</param>
         public override void OnTick(float dt)
         {
-            if (resourceManager.IsOnResourceSite)
+            if (IsAtResourceSite())
             {
-                if (resourceManager.GetResourceAmount(agentType.GetAgentType()) > _resourceTargetAmount)
-                {
-                    if (!moveSystem.IsMoving)
-                    {
-                        moveSystem.MoveTo(AreaMover.Destination.WorkingArea);
-                    }
-                }
+                CheckResourceThresholdAndTransition();
             }
         }
+        #endregion
+
+        #region Private Methods
+        /// <summary>
+        /// Moves the agent to the designated resource gathering area.
+        /// </summary>
+        private void MoveToGatheringArea()
+        {
+            moveSystem.MoveTo(AreaMover.Destination.GatheringArea);
+        }
+
+        /// <summary>
+        /// Checks if the agent is currently at a resource collection site.
+        /// </summary>
+        private bool IsAtResourceSite()
+        {
+            return resourceManager.IsOnResourceSite;
+        }
+
+        /// <summary>
+        /// Evaluates current resource levels and transitions to working area if threshold is met.
+        /// </summary>
+        private void CheckResourceThresholdAndTransition()
+        {
+            float currentResources = resourceManager.GetResourceAmount(agentType.GetAgentType());
+            
+            if (HasSufficientResources(currentResources) && !moveSystem.IsMoving)
+            {
+                TransitionToWorkingArea();
+            }
+        }
+
+        /// <summary>
+        /// Determines if the agent has collected enough resources to proceed to work.
+        /// </summary>
+        private bool HasSufficientResources(float currentAmount)
+        {
+            return currentAmount > resourceTargetAmount;
+        }
+
+        /// <summary>
+        /// Initiates movement to the working area for resource processing.
+        /// </summary>
+        private void TransitionToWorkingArea()
+        {
+            moveSystem.MoveTo(AreaMover.Destination.WorkingArea);
+        }
+        #endregion
     }
 }
+
--- Towergeist/Assets/_Dyson/Scripts/GOAP/GoalCollect.cs
+++ Towergeist/Assets/_Dyson/Scripts/GOAP/GoalCollect.cs
@@ -1,83 +1,158 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using JW.Grid.GOAP.Goals;
 using Resources;
+using UnityEngine;
 
-public class GoalCollect : GoalBase
+
+
+namespace Dyson.Towergeist
 {
-    [SerializeField] private float collectPriority = 1f;
-    private float currentPriority = 1f;
-    [SerializeField] private float priorityBuildRate = 1f;
-    [SerializeField] private float priorityDecayRate = .5f;
-    [SerializeField] private float priorityMultiplier = 10f;
-    [SerializeField] private ResourceManager _resourceManager;
-    [SerializeField] private float lowThreshold = 5f;
-    public ResourceManager.ResourceType CurrentTargetResource { get; private set; }
-    
-    public bool restrictToSpecificResource = false;
-    public ResourceManager.ResourceType specificResource;
-    public override void OnGoalActivated()
-    {   
-        currentPriority = collectPriority;
-        //isGoalActivated = true;
-    }
-    public override int CalculatePriority()
+    /// <summary>
+    /// GOAP Goal for resource collection behavior.
+    /// Dynamically prioritizes collection of the most scarce resource type.
+    /// </summary>
+    public class GoalCollect : GoalBase
     {
-        float amount = _resourceManager.GetResourceAmount(CurrentTargetResource);
-        ResourceManager.ResourceType target = restrictToSpecificResource ? specificResource : CurrentTargetResource;
-        if (amount >= lowThreshold)
+        #region Serialized Fields
+
+        [Header("Priority Configuration")] [SerializeField]
+        private float collectPriority = 1f;
+
+        [SerializeField] private float priorityBuildRate = 1f;
+        [SerializeField] private float priorityDecayRate = 0.5f;
+        [SerializeField] private float priorityMultiplier = 10f;
+        [SerializeField] private float lowThreshold = 5f;
+
+        [Header("Resource Management")] [SerializeField]
+        private ResourceManager resourceManager;
+
+        [Header("Resource Targeting")] [SerializeField]
+        private bool restrictToSpecificResource = false;
+
+        [SerializeField] private ResourceManager.ResourceType specificResource;
+
+        #endregion
+
+        #region Private Fields
+
+        private float currentPriority = 1f;
+
+        #endregion
+
+        #region Public Properties
+
+        public ResourceManager.ResourceType CurrentTargetResource { get; private set; }
+
+        #endregion
+
+        #region Goal Lifecycle
+
+        public override void OnGoalActivated()
         {
-            return 0;
+            currentPriority = collectPriority;
         }
-        return Mathf.FloorToInt((lowThreshold - amount) * priorityMultiplier);
+
+        public override void OnGoalTick()
+        {
+            DetermineTargetResource();
+        }
+
+        public override bool CanRun()
+        {
+            GoalCanRun = resourceManager != null;
+            return GoalCanRun;
+        }
+
+        #endregion
+
+        #region Priority Calculation
+
+        public override int CalculatePriority()
+        {
+            ResourceManager.ResourceType targetResource = GetTargetResourceType();
+            float currentAmount = resourceManager.GetResourceAmount(targetResource);
+
+            if (currentAmount >= lowThreshold)
+            {
+                return 0;
+            }
+
+            float scarcityFactor = lowThreshold - currentAmount;
+            return Mathf.FloorToInt(scarcityFactor * priorityMultiplier);
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Determines which resource type should be targeted for collection.
+        /// Prioritizes the resource with the lowest current amount.
+        /// </summary>
+        private void DetermineTargetResource()
+        {
+            if (restrictToSpecificResource)
+            {
+                CurrentTargetResource = specificResource;
+                return;
+            }
+
+            var resourceAmounts = GetAllResourceAmounts();
+            CurrentTargetResource = FindLowestResource(resourceAmounts);
+        }
+
+        /// <summary>
+        /// Gets the current amounts of all resource types.
+        /// </summary>
+        private ResourceAmounts GetAllResourceAmounts()
+        {
+            return new ResourceAmounts
+            {
+                Stone = resourceManager.GetResourceAmount(ResourceManager.ResourceType.Stone),
+                Wood = resourceManager.GetResourceAmount(ResourceManager.ResourceType.Wood),
+                Cement = resourceManager.GetResourceAmount(ResourceManager.ResourceType.Cement)
+            };
+        }
+
+        /// <summary>
+        /// Finds the resource type with the lowest current amount.
+        /// </summary>
+        private ResourceManager.ResourceType FindLowestResource(ResourceAmounts amounts)
+        {
+            if (amounts.Stone <= amounts.Wood && amounts.Stone <= amounts.Cement)
+            {
+                return ResourceManager.ResourceType.Stone;
+            }
+
+            if (amounts.Wood <= amounts.Stone && amounts.Wood <= amounts.Cement)
+            {
+                return ResourceManager.ResourceType.Wood;
+            }
+
+            return ResourceManager.ResourceType.Cement;
+        }
+
+        /// <summary>
+        /// Gets the appropriate target resource type based on current settings.
+        /// </summary>
+        private ResourceManager.ResourceType GetTargetResourceType()
+        {
+            return restrictToSpecificResource ? specificResource : CurrentTargetResource;
+        }
+
+        #endregion
+
+        #region Helper Structures
+
+        /// <summary>
+        /// Container for resource amounts to improve code readability.
+        /// </summary>
+        private struct ResourceAmounts
+        {
+            public float Stone;
+            public float Wood;
+            public float Cement;
+        }
+
+        #endregion
     }
-
-    public override void OnGoalTick()
-    {
-        float stone = _resourceManager.GetResourceAmount(ResourceManager.ResourceType.Stone);
-        float wood = _resourceManager.GetResourceAmount(ResourceManager.ResourceType.Wood);
-        float cement = _resourceManager.GetResourceAmount(ResourceManager.ResourceType.Cement);
-
-       // float lowest = Mathf.Min(wood, stone, cement);
-
-       if (restrictToSpecificResource)
-       {
-           CurrentTargetResource = specificResource;
-       }
-        if (stone <= wood & stone <= cement)
-        {
-            CurrentTargetResource = ResourceManager.ResourceType.Stone;
-        }
-        else if (wood <= stone && wood <= cement)
-        {
-            CurrentTargetResource = ResourceManager.ResourceType.Wood;
-        }
-        else
-        {
-            CurrentTargetResource = ResourceManager.ResourceType.Cement;
-        }
-        /*
-        if (stone <= lowStoneThreshold)
-        {
-            // Raise priority over time while low on stone
-            currentPriority += priorityBuildRate * Time.fixedDeltaTime;
-        }
-        else
-        {
-            // Decay priority when stone is enough
-            currentPriority -= priorityDecayRate * Time.fixedDeltaTime;
-        }
-
-        // Clamp to avoid negatives or overflow
-        currentPriority = Mathf.Clamp(currentPriority, 0f, 100f); */
-    }
-    
-    public override bool CanRun()
-    {
-        GoalCanRun = true;
-        return GoalCanRun;
-    }
-
 }
